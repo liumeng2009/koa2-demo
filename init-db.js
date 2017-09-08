b85df3a51dae91d84a1ffcc46fded7a1b2a85834
liumeng <378338627@qq.com>
2017-09-08 17:26:32 +0800

    201709081726
    201709081726

--- init-db.js
+++ init-db.js
@@ -31,10 +31,38 @@ model.sync().then(()=>{
         status:1
     }).then(function(p){
         console.log('created'+JSON.stringify(p)+'test the password');
-        process.exit(0);
     }).catch(function(err){
         console.log('failed:'+err);
     });
+
+    //插入初始化的组织信息
+    let Group=model.groups;
+    Group.create({
+        name:'管委会',
+        description:'group1',
+        status:1
+    }).then(function(result){
+        console.log('group1 initital success'+result.id);
+    }).catch(function(err){
+        console.log('group1 failed:'+err);
+    });
+
+    Group.create({
+        name:'控股公司',
+        description:'group2',
+        status:1
+    }).then(function(result){
+        console.log('group2 initital success'+result);
+    }).catch(function(err){
+        console.log('group2 failed:'+err);
+    });
+
+
+
+
+
+
+
 }).catch((e)=>{
     console.log('failed with: '+e);
     process.exit(0);});
