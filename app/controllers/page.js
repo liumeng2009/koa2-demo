4d3666085c0cde07194b198ac37ae607480ed934
liumeng <378338627@qq.com>
2018-04-03 17:29:00 +0800

    201804031728

--- app/controllers/page.js
+++ app/controllers/page.js
@@ -5,39 +5,174 @@ var stream =require('stream');
 
 
 exports.operation=async(ctx,next)=>{
-   ctx.res.writeHead ( 200, {
-        "Content-Type": "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-        'Content-disposition': 'attachment; filename=surprise.pptx'
-   });
-    var pptx = officegen ( 'pptx' );
+    let id=ctx.params.id;
+    let date=new Date();
+    let dateStamp=date.getTime();
 
-    pptx.on ( 'finalize', function ( written ) {
+   ctx.res.writeHead ( 200, {
+        "Content-Type": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        'Content-disposition': 'attachment; filename=WorkerOrder_'+dateStamp+'.docx'
+   });
+    var docx = officegen ({
+        type: 'docx',
+        orientation: 'portrait',
+        pageMargins: { top: 1000, left: 1000, bottom: 1000, right: 1000 }
+    });
+
+    docx.on ( 'finalize', function ( written ) {
         console.log ( 'Finish to create the surprise PowerPoint stream and send it to '  + 'Total bytes created: ' + written + '\n' );
     });
 
-    pptx.on ( 'error', function ( err ) {
+    docx.on ( 'error', function ( err ) {
         console.log ( err );
     });
 
-    var slide = pptx.makeNewSlide();
-    slide.back = '000000';
-    slide.color = 'ffffff';
+    var pObj = docx.createP ();
 
-    slide.addText ( 'Hello ' + '!', { y: 20, cx: '100%', font_size: 56, font_face: 'Arial', bold: true, color: 'ffff00', align: 'center' } );
+    pObj.addText ( 'Simple' );
+    pObj.addText ( ' with color', { color: '000088' } );
+    pObj.addText ( ' and back color.', { color: '00ffff', back: '000088' } );
 
-    slide.addText ( 'Requested URL', { y: 150, cx: '50%' } );
-    console.log(ctx.req.url)
-    slide.addText ( ctx.req.url, { y: 150, x: '50%', cx: '50%', color: '0000ff' } );
-    slide.addText ( 'Request Method', { y: 180, cx: '50%' } );
-    console.log(ctx.req.method);
-    slide.addText ( ctx.req.method, { y: 180, x: '50%', cx: '50%', color: '0000ff' } );
-    slide.addText ( 'Request Dara', { y: 210, cx: '50%' } );
-    slide.addText ( 'name', { y: 210, x: '50%', cx: '50%', color: '0000ff' } );
+    var pObj = docx.createP ();
 
-    //var out = fs.createWriteStream ( './tmp/out.pptx' );
+    pObj.addText ( 'Since ' );
+    pObj.addText ( 'officegen 0.2.12', { back: '00ffff', shdType: 'pct12', shdColor: 'ff0000' } ); // Use pattern in the background.
+    pObj.addText ( ' you can do ' );
+    pObj.addText ( 'more cool ', { highlight: true } ); // Highlight!
+    pObj.addText ( 'stuff!', { highlight: 'darkGreen' } ); // Different highlight color.
+
+    var pObj = docx.createP ();
+
+    pObj.addText ('Even add ');
+    pObj.addText ('external link', { link: 'https://github.com' });
+    pObj.addText ('!');
+
+    var pObj = docx.createP ();
+
+    pObj.addText ( 'Bold + underline', { bold: true, underline: true } );
+
+    var pObj = docx.createP ( { align: 'center' } );
+
+    pObj.addText ( 'Center this text', { border: 'dotted', borderSize: 12, borderColor: '88CCFF' } );
+
+    var pObj = docx.createP ();
+    pObj.options.align = 'right';
+
+    pObj.addText ( 'Align this text to the right.' );
+
+    var pObj = docx.createP ();
+
+    pObj.addText ( 'Those two lines are in the same paragraph,' );
+    pObj.addLineBreak ();
+    pObj.addText ( 'but they are separated by a line break.' );
+
+    docx.putPageBreak ();
+
+    var pObj = docx.createP ();
+
+    pObj.addText ( 'Fonts face only.', { font_face: 'Arial' } );
+    pObj.addText ( ' Fonts face and size.', { font_face: 'Arial', font_size: 40 } );
+
+    docx.putPageBreak ();
+
+    var pObj = docx.createP ();
+
+    pObj.addImage ( path.resolve(__dirname, 'images_for_examples/image3.png' ) );
+
+    docx.putPageBreak ();
+
+    var pObj = docx.createP ();
+
+    pObj.addImage ( path.resolve(__dirname, 'images_for_examples/image1.png' ) );
+
+    var pObj = docx.createP ();
+
+    pObj.addImage ( path.resolve(__dirname, 'images_for_examples/sword_001.png' ) );
+    pObj.addImage ( path.resolve(__dirname, 'images_for_examples/sword_002.png' ) );
+    pObj.addImage ( path.resolve(__dirname, 'images_for_examples/sword_003.png' ) );
+    pObj.addText ( '... some text here ...', { font_face: 'Arial' } );
+    pObj.addImage ( path.resolve(__dirname, 'images_for_examples/sword_004.png' ) );
+
+    var pObj = docx.createP ();
+
+    pObj.addImage ( path.resolve(__dirname, 'images_for_examples/image1.png' ) );
+
+    docx.putPageBreak ();
+
+    var pObj = docx.createListOfNumbers ();
+
+    pObj.addText ( 'Option 1' );
+
+    var pObj = docx.createListOfNumbers ();
+
+    pObj.addText ( 'Option 2' );
+
+    pObj.addHorizontalLine ();
+
+    var pObj = docx.createP ({ backline: 'E0E0E0' });
+
+    pObj.addText ( 'Backline text1' );
+
+    pObj.addText ( ' text2' );
+
+    var table = [
+        [{
+            val: "No.",
+            opts: {
+                cellColWidth: 4261,
+                b:true,
+                sz: '48',
+                shd: {
+                    fill: "7F7F7F",
+                    themeFill: "text1",
+                    "themeFillTint": "80"
+                },
+                fontFamily: "Avenir Book"
+            }
+        },{
+            val: "Title1",
+            opts: {
+                b:true,
+                color: "A00000",
+                align: "right",
+                shd: {
+                    fill: "92CDDC",
+                    themeFill: "text1",
+                    "themeFillTint": "80"
+                }
+            }
+        },{
+            val: "Title2",
+            opts: {
+                align: "center",
+                cellColWidth: 42,
+                b:true,
+                sz: '48',
+                shd: {
+                    fill: "92CDDC",
+                    themeFill: "text1",
+                    "themeFillTint": "80"
+                }
+            }
+        }],
+        [1,'All grown-ups were once children',''],
+        [2,'there is no harm in putting off a piece of work until another day.',''],
+        [3,'But when it is a matter of baobabs, that always means a catastrophe.',''],
+        [4,'watch out for the baobabs!','END'],
+    ]
+
+    var tableStyle = {
+        tableColWidth: 4261,
+        tableSize: 24,
+        tableColor: "ada",
+        tableAlign: "left",
+        tableFontFamily: "Comic Sans MS"
+    }
+
+    var pObj = docx.createTable (table, tableStyle);
 
     let r=ctx.res;
-    pptx.generate ( r);
+    docx.generate ( r);
     ctx.body=r;
 
 }
